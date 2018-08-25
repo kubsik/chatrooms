23156c6c9b0c75b211e79a622d063cf3f80d05a5
kubsik <almagra.mk@gmail.com>
2018-08-25 08:22:51 +0200

    [socketIO] move listen function

--- server.js
+++ server.js
@@ -57,21 +57,7 @@ server.listen(3000, function() {
 	console.log("Serwer nasłuchuje na porcie 3000.");
 });
 
-exports.listen = function(server) {
-	io = socketio.listen(server); //Uruchomienie serwera Socket.IO i umożliwienie mu współpracy z istniejącym serwerem HTTP. 
-	io.set('log level', 1);
-	io.sockets.on('connection', function (socket) {  //Zdefiniowanie sposobu obsłu
-		guestNumber = assignGuestName(socket, guestNumber,     nickNames, namesUsed);//  Przypisanie użytkownikowi nazwy gościa podczas nawiązywania połączenia. 
-		joinRoom(socket, 'Lobby');  //Umieszczenie użytkownika w pokoju Lobby, gdy próbuje on nawiązać połączenie. 
-		handleMessageBroadcasting(socket, nickNames); // Obsługa wiadomości użytkownika, prób zmiany nazwy użytkownika, a także tworzenia lub zmiany pokoju czatu. 
-		handleNameChangeAttempts(socket, nickNames, namesUsed);
-		handleRoomJoining(socket);
-		socket.on('rooms', function() { // Wyświetlenie użytkownika wraz z listą pokoi, w których prowadzi czat. 
-			socket.emit('rooms', io.sockets.manager.rooms);
-		});
-		handleClientDisconnection(socket, nickNames, namesUsed); // Zdefiniowanie logiki wykonywanej podczas rozłączania użytkownika. 
-	});
-}; 
+
   
   
   
